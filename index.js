9716d2a5bb92ad02aae0c1892eddb90f35b7edd9
Althaf <althafahammedm626@gmail.com>
2024-07-23 15:32:04 +0530

    exercise 3.4

--- index.js
+++ index.js
@@ -1,48 +1,55 @@
-// console.log('hello world');
-const express = require('express')
-const app = express()
+const express = require("express");
+const app = express();
 
 const phonebooks = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+  {
+    id: "1",
+    name: "Arto Hellas",
+    number: "040-123456",
+  },
+  {
+    id: "2",
+    name: "Ada Lovelace",
+    number: "39-44-5323523",
+  },
+  {
+    id: "3",
+    name: "Dan Abramov",
+    number: "12-43-234345",
+  },
+  {
+    id: "4",
+    name: "Mary Poppendieck",
+    number: "39-23-6423122",
+  },
+];
 
-app.get('/api/persons',(request,response)=> {
-    response.json(phonebooks)
-})
+app.get("/api/persons", (request, response) => {
+  response.json(phonebooks);
+});
 
-app.get('/info',(requset,response)=> {
-    response.send(`Phonebook has info of ${phonebooks.length} people <br> <br> ${Date()}`)
-})
+app.get("/info", (requset, response) => {
+  response.send(
+    `Phonebook has info of ${phonebooks.length} people <br> <br> ${Date()}`
+  );
+});
 
-app.get('/api/persons/:id',(request,response)=> {
-  const id = request.params.id
-  const person = phonebooks.filter((Phonebook)=> Phonebook.id === id)
-  if(person.length === 0) {
-    return response.status(404).json({error: '404 Not Found' })
+app.get("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const person = phonebooks.filter((Phonebook) => Phonebook.id === id);
+  if (person.length === 0) {
+    return response.status(404).json({ error: "404 Not Found" });
   }
-  response.json(person)
-})
+  response.json(person);
+});
 
-const PORT = 3001
-app.listen(PORT,()=> {
-    console.log(`port is running on ${PORT}`);
-})
+app.delete("/api/persons/:id", (request, response) => {
+  const id = request.params.id;
+  const persons = phonebooks.filter((phonebook) => phonebook.id != id);
+  response.status(204).end();
+});
+
+const PORT = 3001;
+app.listen(PORT, () => {
+  console.log(`port is running on ${PORT}`);
+});
