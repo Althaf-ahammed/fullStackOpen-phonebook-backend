e8715720ff1bdd0993c046e2244b761c010bdd67
Althaf <althafahammedm626@gmail.com>
2024-08-05 16:03:58 +0530

    exercise 3.16

--- index.js
+++ index.js
@@ -38,10 +38,11 @@ let phonebooks = [
   },
 ];
 
-app.get("/api/persons", (request, response) => {
+app.get("/api/persons", (request, response,next) => {
   Person.find({}).then((result) => {
     response.json(result);
-  });
+  })
+  .catch(error => next(error))
 });
 
 app.get("/info", (requset, response) => {
@@ -59,14 +60,15 @@ app.get("/api/persons/:id", (request, response) => {
   response.json(person);
 });
 
-app.delete("/api/persons/:id", (request, response) => {
+app.delete("/api/persons/:id", (request, response,next) => {
   const id = request.params.id;
   Person.findByIdAndDelete(id).then(result=> {
     response.status(204).end()
   })
+  .catch(error => next(error))
 });
 
-app.post("/api/persons", (request, response) => {
+app.post("/api/persons", (request, response,next) => {
   const body = request.body;
 
   // if (!body.name) {
@@ -90,7 +92,18 @@ app.post("/api/persons", (request, response) => {
   person.save().then(savedPerson => {
     response.json(savedPerson)
   })
+  .catch(error => next(error))
 });
+
+const errorHandler = (error,request,response,next) => {
+console.error(error.message);
+
+if(error.name === 'CastError') {
+  return response.status(400).send({ error: 'malformatted id' })
+}
+next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT;
 app.listen(PORT, () => {
